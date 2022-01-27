8f09ff550c31ea7f2932e0a899a03218b936cf31
timlokers <39838876+timlokers@users.noreply.github.com>
2022-01-27 16:51:03 +0100

    27-01-2022: Try to add sound effect

--- lib/Logic/Controllers/bitrate_controller.dart
+++ lib/Logic/Controllers/bitrate_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
 
 class BitrateController extends GetxController{
@@ -14,12 +15,15 @@ class BitrateController extends GetxController{
       count++;
       if(count == 1){
         imageString = './Resources/Images/knobs/KnobBitrate_128_mini.png';
+        //playBitrateSound(false);
       }
       else if(count == 2){
         imageString = './Resources/Images/knobs/KnobBitrate_192_mini.png';
+        //playBitrateSound(false);
       }
       else{
         imageString = './Resources/Images/knobs/KnobBitrate_320_mini.png';
+        //playBitrateSound(true);
       }
     }
     update();
@@ -28,4 +32,15 @@ class BitrateController extends GetxController{
   }
 
 
+}
+
+//To do: Make it work
+Future<AudioPlayer> playBitrateSound(bool isEnd) async{
+  AudioCache cache = AudioCache();
+  if(isEnd){
+    return await cache.play("SwitchOff.wav");
+  }
+  else{
+    return await cache.play("SwitchOn.wav");
+  }
 }
